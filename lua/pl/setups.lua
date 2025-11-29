638d3519c5fc12c87c415171c918824b6a3aae1e
Otavio Silva <ot4vi0silv4@gmail.com>
2025-11-29 00:51:01 -0300

    adicionando suporte a imagens

--- lua/pl/setups.lua
+++ lua/pl/setups.lua
@@ -53,3 +53,19 @@ require("markdown").setup({})
 --     },
 -- })
 
+require("image").setup({
+    backend = "kitty", -- or "ueberzug" or "sixel"
+    processor = "magick_cli", -- or "magick_rock"
+    integrations = {
+        markdown = {
+            enabled = true,
+            clear_in_insert_mode = false,
+            download_remote_images = true,
+            only_render_image_at_cursor = false,
+            only_render_image_at_cursor_mode = "popup", -- or "inline"
+            floating_windows = false, -- if true, images will be rendered in floating markdown windows
+            filetypes = { "markdown", "vimwiki" }, -- markdown extensions (ie. quarto) can go here
+        },
+    },
+})
+
