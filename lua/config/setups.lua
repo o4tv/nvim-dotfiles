728a72018aab11e74f90dd5a1e7517cacde0c8c6
Otavio Silva <ot4vi0silv4@gmail.com>
2025-11-30 05:18:29 -0300

    adicionando lualine

--- lua/config/setups.lua
+++ lua/config/setups.lua
@@ -154,3 +154,13 @@ require('gitsigns').setup{
   end
 }
 
+require("lualine").setup({
+    options = {
+        theme = 'ayu_mirage',
+        globalstatus = true,
+    },
+    sections = {
+        lualine_x = {'buffers', 'encoding', 'fileformat', 'filetype'},
+    }
+})
+
