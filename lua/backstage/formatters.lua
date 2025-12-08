4620abab1a48473a08e6359ef251610d4e9c5e91
Otavio Silva <ot4vi0silv4@gmail.com>
2025-12-08 17:40:08 -0300

    removendo warning

--- lua/backstage/formatters.lua
+++ lua/backstage/formatters.lua
@@ -1,7 +1,7 @@
 require('conform').setup({
     formatters_by_ft = {
         lua = { 'stylua' },
-        javascript = { 'prettierd', 'prettier', stop_after_first = true },
-        typescript = { 'prettierd', 'prettier', stop_after_first = true },
+        javascript = { 'prettier', stop_after_first = true },
+        typescript = { 'prettier', stop_after_first = true },
     },
 })
