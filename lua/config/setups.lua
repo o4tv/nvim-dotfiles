c057793f88e914c18973182d4eaf4db6b1a129c1
o4tv <ot4vi0silv4@gmail.com>
2026-04-08 14:29:44 -0300

    disable image setup

--- lua/config/setups.lua
+++ lua/config/setups.lua
@@ -57,22 +57,22 @@ require('markdown').setup({
 
 })
 
-require('image').setup({
-    backend = 'kitty', -- or "ueberzug" or "sixel"
-    kitty_method = 'normal',
-    processor = 'magick_cli', -- or "magick_rock"
-    integrations = {
-        markdown = {
-            enabled = true,
-            clear_in_insert_mode = false,
-            download_remote_images = true,
-            only_render_image_at_cursor = false,
-            only_render_image_at_cursor_mode = 'popup', -- or "inline"
-            floating_windows = false, -- if true, images will be rendered in floating markdown windows
-            filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
-        },
-    },
-})
+-- require('image').setup({
+--     backend = 'kitty', -- or "ueberzug" or "sixel"
+--     kitty_method = 'normal',
+--     processor = 'magick_cli', -- or "magick_rock"
+--     integrations = {
+--         markdown = {
+--             enabled = true,
+--             clear_in_insert_mode = false,
+--             download_remote_images = true,
+--             only_render_image_at_cursor = false,
+--             only_render_image_at_cursor_mode = 'popup', -- or "inline"
+--             floating_windows = false, -- if true, images will be rendered in floating markdown windows
+--             filetypes = { 'markdown', 'vimwiki' }, -- markdown extensions (ie. quarto) can go here
+--         },
+--     },
+-- })
 
 require('gitsigns').setup({
     on_attach = function(bufnr)
@@ -187,6 +187,121 @@ require('telescope').setup({
         },
     },
 })
+
+require('cord').setup({
+  enabled = true,
+  log_level = vim.log.levels.OFF,
+  editor = {
+    client = 'neovim',
+    tooltip = 'https://github.com/o4tv',
+    icon = nil,
+  },
+  display = {
+    theme = 'default',
+    flavor = 'dark',
+    view = 'full',
+    swap_fields = false,
+    swap_icons = false,
+  },
+  timestamp = {
+    enabled = true,
+    reset_on_idle = false,
+    reset_on_change = false,
+    shared = false,
+  },
+  idle = {
+    enabled = true,
+    timeout = 30000,
+    show_status = true,
+    ignore_focus = true,
+    unidle_on_focus = true,
+    smart_idle = true,
+    details = '💤',
+    state = nil,
+    tooltip = '💤',
+    icon = nil,
+  },
+  text = {
+    default = nil,
+    workspace = function(opts) return 'in ' .. opts.workspace end,
+    viewing = function(opts) return 'viewing ' .. opts.filename end,
+    editing = function(opts) return 'editing ' .. opts.filename end,
+    file_browser = function(opts) return 'browsing files' end,
+    plugin_manager = function(opts) return 'managing plugins in ' .. opts.name end,
+    lsp = function(opts) return 'configuring LSP in ' .. opts.name end,
+    docs = function(opts) return 'reading ' .. opts.name end,
+    vcs = function(opts) return 'committing changes in ' .. opts.name end,
+    notes = function(opts) return 'taking notes in ' .. opts.name end,
+    debug = function(opts) return 'debugging in ' .. opts.name end,
+    test = function(opts) return 'testing in ' .. opts.name end,
+    diagnostics = function(opts) return 'fixing problems in ' .. opts.name end,
+    games = function(opts) return 'playing ' .. opts.name end,
+    terminal = function(opts) return 'running shit on terminal' end,
+    dashboard = 'home',
+  }, 
+  -- buttons = nil,
+  buttons = {
+    {
+      label = 'view repo',
+      -- url = function(opts) return opts.repo_url end,
+      url = function(opts) return opts.repo_url end,
+    },
+  },
+  assets = nil,
+  variables = nil,
+  hooks = {
+    ready = nil,
+    shutdown = nil,
+    pre_activity = nil,
+    post_activity = nil,
+    idle_enter = nil,
+    idle_leave = nil,
+    workspace_change = nil,
+    buf_enter = nil,
+  },
+  extensions = nil,
+  advanced = {
+    plugin = {
+      autocmds = true,
+      cursor_update = 'on_hold',
+      match_in_mappings = true,
+      debounce = {
+        delay = 50,
+        interval = 750,
+      },
+    },
+    server = {
+      update = 'fetch',
+      pipe_path = nil,
+      executable_path = nil,
+      timeout = 300000,
+    },
+    discord = {
+      pipe_paths = nil,
+      reconnect = {
+        enabled = false,
+        interval = 5000,
+        initial = true,
+      },
+      sync = {
+        enabled = true,
+        mode = 'periodic',
+        interval = 12000,
+        reset_on_update = true,
+        pad = true,
+      },
+    },
+    workspace = {
+      root_markers = {
+        '.git',
+        '.hg',
+        '.svn',
+      },
+      limit_to_cwd = false,
+    },
+  },
+})
+
 require('project_nvim').setup({})
 
 require('uv').setup()
